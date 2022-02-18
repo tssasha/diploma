3197dc70e1b7b14fc39e480fdac123cfcd29c9b7
Sasha <sashas.p@ya.ru>
2022-02-18 17:31:07 +0300

    Flask added

--- clusterer.py
+++ clusterer.py
@@ -22,14 +22,14 @@ class Clusterer:
     nlp = spacy.load("ru_core_news_sm")
 
     def __init__(self):
-        self.id = 1
+        self.cur_id = 1
         self.fitted_dicts = None
         self.clusters = None
         self.generate_dicts()
 
     def generate_dicts(self):
         df = select_to_df('select * from news where id >= 1 and id <= 100')
-        self.id = 100
+        self.cur_id = 100
         tokens_df = pd.DataFrame()
         tokens_df['title_nlp'] = df['title'].map(self.nlp)
         tokens_df['body_nlp'] = df['text'].map(self.nlp)
@@ -52,8 +52,8 @@ class Clusterer:
                              TfidfVectorizer().fit(tokens_df['t_entities'] + ' ' + tokens_df['b_entities'])]
 
     def clusterize_one(self):
-        self.id += 1
-        row = select_by_id(self.id)
+        self.cur_id += 1
+        row = select_by_id(self.cur_id)
         title = self.nlp(row['title'])
         body = self.nlp(row['text'])
         title_body = self.nlp(row['title'] + ' ' + row['text'])
@@ -64,30 +64,21 @@ class Clusterer:
         mtx = [self.fitted_dicts[j].transform([doc[j]]).toarray().flatten() for j in range(9)]
 
         if self.clusters is None:
-            self.clusters = [(mtx, [self.id])]
-            return
+            self.clusters = [(mtx, [self.cur_id])]
+            return 0
 
         cscores = [cscore(cluster[0], mtx) for cluster in self.clusters]
         max_cscore = np.max(cscores)
 
         if max_cscore < 1.5:
-            self.clusters.append((mtx, [self.id]))
-        else:
-            cl_arg = np.argmax(cscores)
-            cur_cluster = self.clusters[cl_arg]
-            cl_size = len(cur_cluster[1])
-            for i in range(9):
-                self.clusters[cl_arg][0][i] = (cur_cluster[0][i] * cl_size + mtx[i]) / (cl_size + 1)
-            self.clusters[cl_arg][1].append(self.id)
+            self.clusters.append((mtx, [self.cur_id]))
+            return len(self.clusters) - 1
 
+        cl_arg = np.argmax(cscores)
+        cur_cluster = self.clusters[cl_arg]
+        cl_size = len(cur_cluster[1])
+        for i in range(9):
+            self.clusters[cl_arg][0][i] = (cur_cluster[0][i] * cl_size + mtx[i]) / (cl_size + 1)
+        self.clusters[cl_arg][1].append(self.cur_id)
+        return cl_arg
 
-a = Clusterer()
-print("Clusterer initialized")
-for i in range(50):
-    a.clusterize_one()
-
-print(len(a.clusters))
-for cluster in a.clusters:
-    print(cluster[1])
-    for c in cluster[1]:
-        print(select_by_id(c)['title'])
---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect
+from clusterer import Clusterer
+from db.db_tools import select_by_id
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("main.html", news_id=None, news_title=None, result=None)
+
+
+@app.route('/clusterize_one/', methods=["POST"])
+def clusterize_one():
+    ans = clusterer.clusterize_one()
+    news_title = select_by_id(clusterer.cur_id)['title']
+    return render_template("main.html", news_id=clusterer.cur_id, news_title=news_title, result=str(ans))
+
+
+if __name__ == '__main__':
+    print("Clusterer initialization in progress...")
+    clusterer = Clusterer()
+    print("Clusterer initialized")
+    app.run(host='127.0.0.1', port=8000)
